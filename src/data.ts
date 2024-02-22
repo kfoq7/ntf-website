e5072f1d2b8619914a3e1ef1ebc2ef5a8f81d1a7
kfoq7 <frbrizoi@gmail.com>
2024-02-21 23:01:12 -0500

    feat(*): working in project!

---
+++ src/data.ts
@@ -0,0 +1,105 @@
+import WalletIcon from '@/icons/Wallet.astro'
+import CartIcon from '@/icons/Cart.astro'
+import BoxGridIcon from '@/icons/BoxGrid.astro'
+
+export const info = [
+  {
+    title: 'Connect Wallet',
+    Icon: WalletIcon,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusantium maiores mollitia ipsam iure! Illo ad temporibus velit a? Quisquam voluptate sunt quis corrupti necessitatibus dolor. Facilis quisquam debitis cumque.'
+  },
+  {
+    title: 'NTF Marketplace',
+    Icon: CartIcon,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusantium maiores mollitia ipsam iure! Illo ad temporibus velit a? Quisquam voluptate sunt quis corrupti necessitatibus dolor. Facilis quisquam debitis cumque.'
+  },
+  {
+    title: 'Launch Apps',
+    Icon: BoxGridIcon,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusantium maiores mollitia ipsam iure! Illo ad temporibus velit a? Quisquam voluptate sunt '
+  }
+]
+
+export const categories = [
+  {
+    name: 'ProgGraph',
+    link: '#'
+  },
+  {
+    name: 'Abstract',
+    link: '#'
+  },
+  {
+    name: 'Monkey',
+    link: '#'
+  },
+  {
+    name: 'Cars',
+    link: '#'
+  },
+  {
+    name: 'Trending',
+    link: '#'
+  }
+]
+
+export const crypts = [
+  {
+    title: 'CryptoArt',
+    price: 0.29,
+    items: 5,
+    totalItems: 33,
+    time: '2h 28min 1s',
+    thumbnail: '/assets/item-1.png'
+  },
+  {
+    title: 'CryptoArt',
+    price: 0.29,
+    items: 5,
+    totalItems: 33,
+    time: '2h 28min 1s',
+    thumbnail: '/assets/item-2.png'
+  },
+  {
+    title: 'CryptoArt',
+    price: 0.29,
+    items: 5,
+    totalItems: 33,
+    time: '2h 28min 1s',
+    thumbnail: '/assets/item-3.png'
+  },
+  {
+    title: 'CryptoArt',
+    price: 0.29,
+    items: 5,
+    totalItems: 33,
+    time: '2h 28min 1s',
+    thumbnail: '/assets/item-4.png'
+  }
+]
+
+export const sellers = [
+  {
+    name: 'Jhone D',
+    amount: '15.2k',
+    profilePhoto: '/assets/profile-1.png'
+  },
+  {
+    name: 'Gregory B',
+    amount: '12.2k',
+    profilePhoto: '/assets/profile-2.png'
+  },
+  {
+    name: 'Stephen E D',
+    amount: '7.9k',
+    profilePhoto: '/assets/profile-3.png'
+  },
+  {
+    name: 'Russell M',
+    amount: '4.2k',
+    profilePhoto: '/assets/profile-4.png'
+  }
+]
